3bedd7c305652a85b9cf55b63461ecb789a9f116
Vanesa <Vanelobri123@gmail.com>
2026-08-14 16:20:16 -0600

    linea-4 B

--- historia.py
+++ historia.py
@@ -3,4 +3,3 @@ print("Sofía: Por fin terminé de configurar NOVA. Según esto, puede organizar
 print("Lalo: ¿Predecir? A ver, NOVA, ¿cuándo vamos a terminar la práctica de programación?")
 print("NOVA: La práctica será terminada a las 6:42 p. m.")
 print("Sofía: Bueno... bastante específica.")
-
