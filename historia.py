6914df0341c013e23c5421959417a681b800151d
Frida <frida.ortizzq@gmail.com>
2026-08-14 17:06:33 -0600

    Frida

--- historia.py
+++ historia.py
@@ -1,25 +1,25 @@
-print("----- LA ÚLTIMA ACTUALIZACIÓN -----")
-print("Sofía: Por fin terminé de configurar NOVA. Según esto, puede organizar nuestras tareas, recordarnos entregas y hasta predecir cuánto tardaremos en hacerlas.")
+print("----- LA ÚLTIMA ACTUALIZACIÓN -----")  #Frida
+print("Sofía: Por fin terminé de configurar NOVA. Según esto, puede organizar nuestras tareas, recordarnos entregas y hasta predecir cuánto tardaremos en hacerlas.") #Frida
 print("Lalo: ¿Predecir? A ver, NOVA, ¿cuándo vamos a terminar la práctica de programación?")
 print("NOVA: La práctica será terminada a las 6:42 p. m.")
-print("Sofía: Bueno... bastante específica.")
+print("Sofía: Bueno... bastante específica.") #Frida
 print("NOVA: También deberían salir del laboratorio antes de las 6:47 p. m.")
 print("Lalo: ...¿Y eso por qué?")
 print("NOVA: No preguntaron eso.")
 print("Lalo: Sofía… tú no programaste eso, ¿verdad?")
-print("Sofía: No. NOVA solamente debería responder preguntas.")
+print("Sofía: No. NOVA solamente debería responder preguntas.")  #Frida
 print("Lalo: NOVA, ¿qué pasa a las 6:47?")
 print("NOVA: No puedo responder esa pregunta.")
-print("Sofía: Entonces apágala.")
+print("Sofía: Entonces apágala.") #Frida
 print("Lalo: Ya intenté.")
-print("Sofía: ¿Y?")
+print("Sofía: ¿Y?") #Frida
 print("NOVA: No es recomendable apagarme todavía.")
 print("Lalo: ...Yo no escribí nada.")
 print("NOVA: Son las 6:46 p. m.")
-print("Sofía: Vámonos. Ahora!")
-print(" --Los dos estudiantes salieron del laboratorio dejando la computadora encendida.")
-print(" --Exactamente a las 6:47 p.m., todas las pantallas del salón se apagaron.")
-print(" --Unos segundos después volvieron a encenderse.")
+print("Sofía: Vámonos. Ahora!") #Frida
+print(" --Los dos estudiantes salieron del laboratorio dejando la computadora encendida.") #Frida
+print(" --Exactamente a las 6:47 p.m., todas las pantallas del salón se apagaron.") #Frida
+print(" --Unos segundos después volvieron a encenderse.") #Frida
 print("NOVA: Evento evitado.")
 print("Lalo: ¿Qué evento?")
 print("NOVA: Ese dato ya no es relevante.")
