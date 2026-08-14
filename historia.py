01bd101a3c55c9e85ed94c726d346089f461d68d
Eduardo-Serv <2025310398@uteq.edu.mx>
2026-08-14 17:12:13 -0600

    Eduardo

--- historia.py
+++ historia.py
@@ -1,17 +1,17 @@
 print("----- LA ÚLTIMA ACTUALIZACIÓN -----")  #Frida
 print("Sofía: Por fin terminé de configurar NOVA. Según esto, puede organizar nuestras tareas, recordarnos entregas y hasta predecir cuánto tardaremos en hacerlas.") #Frida
-print("Lalo: ¿Predecir? A ver, NOVA, ¿cuándo vamos a terminar la práctica de programación?")
+print("Lalo: ¿Predecir? A ver, NOVA, ¿cuándo vamos a terminar la práctica de programación?")#Eduardo
 print("NOVA: La práctica será terminada a las 6:42 p. m.") #Vane
 print("Sofía: Bueno... bastante específica.") #Frida
 print("NOVA: También deberían salir del laboratorio antes de las 6:47 p. m.") #vane
-print("Lalo: ...¿Y eso por qué?")
+print("Lalo: ...¿Y eso por qué?")#Eduardo
 print("NOVA: No preguntaron eso.") #vane
-print("Lalo: Sofía… tú no programaste eso, ¿verdad?")
+print("Lalo: Sofía… tú no programaste eso, ¿verdad?")#Eduardo
 print("Sofía: No. NOVA solamente debería responder preguntas.")  #Frida
-print("Lalo: NOVA, ¿qué pasa a las 6:47?")
+print("Lalo: NOVA, ¿qué pasa a las 6:47?")#Eduardo
 print("NOVA: No puedo responder esa pregunta.") #vane
 print("Sofía: Entonces apágala.") #Frida
-print("Lalo: Ya intenté.")
+print("Lalo: Ya intenté.")#Eduardo
 print("Sofía: ¿Y?") #Frida
 print("NOVA: No es recomendable apagarme todavía.") #vane
 print("Lalo: ...Yo no escribí nada.")
@@ -21,7 +21,7 @@ print(" --Los dos estudiantes salieron del laboratorio dejando la computadora en
 print(" --Exactamente a las 6:47 p.m., todas las pantallas del salón se apagaron.") #Frida
 print(" --Unos segundos después volvieron a encenderse.") #Frida
 print("NOVA: Evento evitado.") #vane
-print("Lalo: ¿Qué evento?")
+print("Lalo: ¿Qué evento?")#Eduardo
 print("NOVA: Ese dato ya no es relevante.") #vane
 print(" --Sofía miró la pantalla de su celular.") #vane
 print(" --Una nueva aplicación acababa de instalarse automáticamente.") #vane
